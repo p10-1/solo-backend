c8013df726876f82f323115672366bbdccbd3eef
g0mg0m <g0mg0m@pukyong.ac.kr>
2024-09-13 11:16:29 +0900

    FEAT: add login controller

--- src/main/java/solo/dev/solobackend/member/controller/MemberController.java
+++ src/main/java/solo/dev/solobackend/member/controller/MemberController.java
@@ -1,4 +1,22 @@
 package solo.dev.solobackend.member.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class MemberController {
+    @GetMapping("/login")
+    public String login() {
+
+//        model.addAttribute("userID", userID);
+        return "login";
+    }
+    @PostMapping("/loginAf")
+    public String loginAf(@RequestParam("userID") String userID, Model model) {
+        model.addAttribute("userID", userID);
+        return "/index";
+    }
 }
