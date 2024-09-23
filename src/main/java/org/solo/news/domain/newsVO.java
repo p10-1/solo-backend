4ea55b9ce924452865cac6218d59476152a324d1
SogCheonBul <jy6443@naver.com>
2024-09-23 09:43:58 +0900

    Bring: origin dev

--- src/main/java/org/solo/news/domain/newsVO.java
+++ src/main/java/org/solo/news/domain/newsVO.java
@@ -1,18 +1,4 @@
 package org.solo.news.domain;
 
-
-import lombok.*;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class NewsVO {
-    private long no;
-    private String title;
-    private String link;
-    private String category;
-    private String author;
-    private String pubDate;
-    private String description;
-
 }
