ea9028627dc0d4933c5236f4716c4189f6fc6e15
Sim yun jae <sim232811@naver.com>
2024-10-03 00:34:23 +0900

    FEAT: asset page 일부 수정

--- src/main/java/org/solo/asset/service/AssetService.java
+++ src/main/java/org/solo/asset/service/AssetService.java
@@ -10,7 +10,7 @@ public interface AssetService {
 
     // 특정 사용자의 자산 데이터를 가져오는 메서드
 
-    List<AssetVO> getAssetData(String userId, int months);
+    List<AssetVO> getAssetData(String userId, int maxMonths);
     // 모든 사용자의 평균 자산을 계산하는 메서드
 
     Map<String, Double> calculateAssetAverages();
