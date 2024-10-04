bd35c927429676bed8c67ee71b20b0a488de13ee
g0mg0m <g0mg0m@pukyong.ac.kr>
2024-10-04 13:34:33 +0900

    Bring: dev

--- src/main/java/org/solo/mypage/controller/MypageController.java
+++ src/main/java/org/solo/mypage/controller/MypageController.java
@@ -33,7 +33,6 @@ public class MypageController {
     @GetMapping("/getAsset")
     public ResponseEntity<AssetVO> getAsset(HttpSession session) {
         String userId = (String) session.getAttribute("userId");
-        System.out.println("getAsset 수행중: " + userId);
 
         try {
             AssetVO asset = mypageService.getAssetData(userId); // 리스트 대신 단일 AssetVO 가져오기
@@ -140,106 +139,13 @@ public class MypageController {
         }
     }
 
-
-    //    @PostMapping("/insertAsset")
-//    public ResponseEntity<String> saveUserData(HttpSession session, @RequestBody Map<String, Object> data) {
-//        String userId = (String) session.getAttribute("userId");
-//        System.out.println("insert 수행중: " + userId);
-//
-//        AssetVO assetData = new AssetVO();
-//
-//        if (userId != null) {
-//            assetData.setUserId(userId);
-//        }
-//        // 데이터 매핑
-//        assetData.setConsume((String) data.get("consume"));
-//        assetData.setCash((Integer) data.get("cash"));
-//        assetData.setStock((Integer) data.get("stock"));
-//        assetData.setProperty((Integer) data.get("property"));
-//        assetData.setDeposit((Integer) data.get("deposit"));
-//
-//        // 대출 정보 설정
-//        assetData.setLoanAmount((Integer) data.get("loanAmount"));
-//        assetData.setLoanPurpose((String) data.get("loanPurpose"));
-//        assetData.setPeriod((Integer) data.get("period"));
-//
-//        // DB 저장
-//        if (mypageService.findAssetData(userId)) {
-//            session.setAttribute("message", "이미 저장된 데이터가 있습니다.");
-//            return ResponseEntity.status(409).body("이미 저장된 데이터가 있습니다.");
-//        }
-//        mypageService.insertAssetData(assetData);
-//
-////        // DB에 저장된 데이터 콘솔에 출력 (확인용)
-////        System.out.println("DB에 저장된 데이터:");
-////        System.out.println("userID: " + assetData.getUserID());
-////        System.out.println("Cash: " + assetData.getCash());
-////        System.out.println("Stock: " + assetData.getStock());
-////        System.out.println("Property: " + assetData.getProperty());
-////        System.out.println("Deposit: " + assetData.getDeposit());
-////        System.out.println("소비유형: " + assetData.getConsume());
-////        System.out.println("대출액: " + assetData.getLoanAmount());
-////        System.out.println("대출 목적: " + assetData.getLoanPurpose());
-////        System.out.println("대출 기간: " + assetData.getPeriod());
-//
-//        session.setAttribute("message", "DB 저장 완료");
-//
-//        return ResponseEntity.ok("저장 완료");
-//    }
-//
-//    //update
-//    @PutMapping("/updateAsset")
-//    public ResponseEntity<String> updateUserData(HttpSession session, @RequestBody Map<String, Object> data) {
-//        String userId = (String) session.getAttribute("userId");
-//        System.out.println("update 수행중:" + userId);
-//
-//        // 넘어온 데이터 출력
-//        System.out.println("수정될 데이터: " + data); // 추가된 부분
-//        AssetVO assetData = new AssetVO();
-//
-//        if (userId != null) {
-//            assetData.setUserId(userId);
-//
-//            // 기존 데이터 조회
-//            AssetVO existingData = mypageService.getAssetData(userId); // 기존 데이터를 가져오는 서비스 메서드
-//
-//            // 데이터 매핑
-//            assetData.setConsume(data.get("consume") != null ? (String) data.get("consume") : existingData.getConsume());
-//            assetData.setCash(data.get("cash") != null ? (Integer) data.get("cash") : existingData.getCash());
-//            assetData.setStock(data.get("stock") != null ? (Integer) data.get("stock") : existingData.getStock());
-//            assetData.setProperty(data.get("property") != null ? (Integer) data.get("property") : existingData.getProperty());
-//            assetData.setDeposit(data.get("deposit") != null ? (Integer) data.get("deposit") : existingData.getDeposit());
-//            // 대출 정보 설정
-//            assetData.setLoanAmount(data.get("loanAmount") != null ? (Integer) data.get("loanAmount") : existingData.getLoanAmount());
-//            assetData.setLoanPurpose(data.get("loanPurpose") != null ? (String) data.get("loanPurpose") : existingData.getLoanPurpose());
-//            assetData.setPeriod(data.get("period") != null ? (Integer) data.get("period") : existingData.getPeriod());
-//
-//            System.out.println("assetData"+assetData);
-//            mypageService.updateAssetData(assetData);
-//            session.setAttribute("message", "정보가 성공적으로 수정되었습니다.");
-//            return ResponseEntity.ok("정보가 성공적으로 수정되었습니다.");
-//        } else {
-//            session.setAttribute("message", "사용자 정보를 찾을 수 없습니다.");
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("사용자 정보를 찾을 수 없습니다.");
-//        }
-//    }
-//
-//
-
-    //    @GetMapping("/{accountIndex}")
-//    public ResponseEntity<?> getById(@PathVariable int accountIndex, HttpSession session) {
-//
-//    }
-
+    // 포인트 출금 메서드
     @PostMapping("/withdraw")
     public ResponseEntity<?> withdrawPoints(@RequestBody MemberVO data,
                                             @RequestParam Integer idx,
                                             HttpSession session) {
-        System.out.println("withdraw요청 들어옴");
-        // 세션에서 userId 가져오기
         String userId = (String) session.getAttribute("userId");
         if (userId != null) {
-            // 서비스 메서드 호출
             return mypageService.withdrawPoints(userId, idx, data.getPoint());
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증되지 않은 사용자");
